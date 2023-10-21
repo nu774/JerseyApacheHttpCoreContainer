213c38847328de101d3e657ade38db91c3bda187
nu774 <honeycomb77@gmail.com>
2023-10-21 16:07:26 +0900

    CharacterGenerator: refactor

--- src/main/java/acme/CharacterGenerator.java
+++ src/main/java/acme/CharacterGenerator.java
@@ -4,9 +4,9 @@ import java.util.Iterator;
 
 public class CharacterGenerator implements Iterable<String>, Iterator<String> {
     final static int COLUMNS = 72;
-    final static char first_letter = ' ';
-    final static char last_letter = '~';
-    char pos = first_letter;
+    final static char FIRST_LETTER = ' ';
+    final static char LAST_LETTER = '~';
+    char pos = FIRST_LETTER;
 
     @Override
     public Iterator<String> iterator() {
@@ -18,14 +18,18 @@ public class CharacterGenerator implements Iterable<String>, Iterator<String> {
     }
     @Override
     public String next() {
-        if (pos++ == last_letter)
-            pos = first_letter;
-        final var data = new char[COLUMNS];
+        pos = increment(pos);
+        final var sb = new StringBuilder();
         char ch = pos;
         for (int i = 0; i < COLUMNS; ++i) {
-            if ((data[i] = ch++) == last_letter)
-                ch = first_letter;
+            sb.append(ch);
+            ch = increment(ch);
         }
-        return new String(data);
+        return sb.toString();
+    }
+    private char increment(char ch) {
+        if (ch++ == LAST_LETTER)
+            ch = FIRST_LETTER;
+        return ch;
     }
 }
